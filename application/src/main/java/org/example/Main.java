f5c0fecb8f9fd7c4178ad8df79963273224e375d
murphyqwek <qwekinaft@list.ru>
2025-05-04 12:40:49 +0300

    committed

---
+++ application/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+import javax.swing.*;
+
+import org.example.form.login.LoginForm;
+import org.example.form.register.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(RegisterForm::new);
+    }
+}
